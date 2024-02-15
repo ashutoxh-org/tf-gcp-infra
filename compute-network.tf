86ebb777de44a3eff98c89a21c3ed75b2665b314
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-02-15 12:15:37 -0500

    Added provision to create multiple VPCs

--- compute-network.tf
+++ compute-network.tf
@@ -1,27 +1,29 @@
 resource "google_compute_network" "vpc" {
-  name                            = "vpc-${random_string.resource_name.result}"
+  count                           = var.vpc_count
+  name                            = "vpc-${count.index}-${random_string.resource_name.result}"
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = "webapp-${random_string.resource_name.result}"
-  ip_cidr_range = var.webapp-subnet-cidr-range
-  region        = var.region
-  network       = google_compute_network.vpc.name
+  count         = var.vpc_count
+  name          = "webapp-${count.index}-${random_string.resource_name.result}"
+  ip_cidr_range = var.webapp_subnet_cidr_ranges[count.index]
+  network       = google_compute_network.vpc[count.index].name
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = "db-${random_string.resource_name.result}"
-  ip_cidr_range = var.db-subnet-cidr-range
-  region        = var.region
-  network       = google_compute_network.vpc.name
+  count         = var.vpc_count
+  name          = "db-${count.index}-${random_string.resource_name.result}"
+  ip_cidr_range = var.db_subnet_cidr_ranges[count.index]
+  network       = google_compute_network.vpc[count.index].name
 }
 
 resource "google_compute_route" "webapp_route" {
-  name             = "route-${random_string.resource_name.result}"
-  dest_range       = var.webapp-egress
-  network          = google_compute_network.vpc.name
-  next_hop_gateway = var.internet-gateway
+  count            = var.vpc_count
+  name             = "route-${count.index}-${random_string.resource_name.result}"
+  dest_range       = var.egress_cidr_blocks[0]
+  network          = google_compute_network.vpc[count.index].name
+  next_hop_gateway = var.default_internet_gateway
 }

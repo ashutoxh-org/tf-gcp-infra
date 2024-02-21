5b34dc50130d8e0724a8a7db5528308aaf73d9a2
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-02-21 10:08:51 -0500

    Code to create a GCE from a custom image

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
   }
 }
 
 provider "google" {
-  project = var.project-name
+  project = var.project_id
   region  = var.deployment_region
 }
--- variables.tf
+++ variables.tf
@@ -4,30 +4,83 @@ resource "random_string" "resource_name" {
   length  = 6
 }
 
-variable "project-name" {
+variable "project_id" {
   type = string
 }
 
-variable "vpc_count" {
-  type = number
+variable "routing_mode" {
+  type = string
 }
 
-variable "webapp_subnet_cidr_ranges" {
-  type = list(string)
+variable "webapp_subnet_cidr_range" {
+  type = string
 }
 
-variable "db_subnet_cidr_ranges" {
-  type = list(string)
+variable "db_subnet_cidr_range" {
+  type = string
 }
 
 variable "deployment_region" {
   type = string
 }
 
-variable "egress_cidr_blocks" {
-  type = list(string)
+variable "egress_cidr_block" {
+  type = string
 }
 
 variable "default_internet_gateway" {
+  type = string
+}
+
+variable "protocol" {
+  type = string
+}
+
+variable "http_port" {
+  type = list(string)
+}
+
+variable "https_port" {
+  type = list(string)
+}
+
+variable "ssh_port" {
+  type = list(string)
+}
+
+variable "app_port" {
+  type = list(string)
+}
+
+
+variable "source_ranges" {
+  type = list(string)
+}
+
+variable "sa_scopes" {
+  type = list(string)
+}
+
+variable "sa_email" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "deployment_zone" {
+  type = string
+}
+
+variable "custom_image" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "disk_type" {
   type = string
 }

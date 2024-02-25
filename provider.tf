c12748058a25da8c36c035ba6f61336ccffbf420
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-02-25 18:42:51 -0500

    Assignment 5: Added DB services and startup script to automatically set DB details in app

--- provider.tf
+++ provider.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>4"
+    }
   }
 }
 
 provider "google" {
+  project = var.project_id
+  region  = var.deployment_region
+}
+
+provider "google-beta" {
   project = var.project_id
   region  = var.deployment_region
 }

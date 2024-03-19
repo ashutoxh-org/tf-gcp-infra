145cd6cff6568d2d4d8338f42341fa04b429d12a
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-03-19 18:10:47 -0400

    Updated TTL and image

--- compute-network.tf
+++ compute-network.tf
@@ -288,7 +288,7 @@ EOT
 resource "google_dns_record_set" "webapp_a_record" {
   name         = "ashutoxh.me."
   type         = "A"
-  ttl          = 1
+  ttl          = 600
   managed_zone = "ashutoxh-me"
   rrdatas      = [google_compute_instance.webapp_instance.network_interface.0.access_config.0.nat_ip]
   depends_on   = [google_compute_instance.webapp_instance]
